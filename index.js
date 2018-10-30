c088a955a82b6554554073a3825d0526533a5e09
deeb33 <datafase@yahoo.com>
2018-10-30 01:09:31 -0700

    Remove dust avoidance for OP_RETURN output
    OP_RETURN outputs don't require a non-dust output value.  If a non-zero
    value is put into the output, the amount becomes unspendable and lost
    forever.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var databutton = {
   build: function(o) {
     datacash.build({ "data": o.data }, function(err, tx) {
-      var dust = 0.00000546;
       var s = tx.outputs[0]._script.toASM();
       var el = o.button.$el;
       var config = o.button;
-      config.outputs = [{ script: s, amount: dust, currency: 'BCH' }];
+      config.outputs = [{ script: s, amount: 0, currency: 'BCH' }];
       if (o.button && o.button.$cash && o.button.$cash.to) {
         o.button.$cash.to.forEach(function(receiver) {
           config.outputs.push({ to: receiver.address, amount: receiver.value/100000000, currency: 'BCH' })
